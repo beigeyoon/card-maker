1ffc51579b9f5118e5465aa170ba0d8126fe8a95
beigeyoon <beige.yoon@gmail.com>
2022-04-14 01:53:10 +0900

    add login component

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import styles from './index.module.css';
 import App from './app';
+import AuthService from './service/auth_service';
+
+const authService = new AuthService();
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App authService={authService} />
   </React.StrictMode>,
   document.getElementById('root')
 );
