042d20db7bdf0390bd1a0909a0fe12de99b0cebe
beigeyoon <beige.yoon@gmail.com>
2022-04-14 23:31:53 +0900

    handle state change on editor

--- src/components/maker/maker.jsx
+++ src/components/maker/maker.jsx
@@ -10,41 +10,41 @@ import styles from './maker.module.css';
 
 const Maker = ({authService}) => {
 
-  const [cards, setCards] = useState([
-    {
-      id: '1',
-      name: 'Yooni',
-      company: 'KUUS',
-      theme: 'dark',
-      title: 'Software Engineer',
-      email: 'yooni@gmail.com',
-      message: 'mango',
-      fileName: 'yooni',
-      fileURL: null
-    },
-    {
-      id: '2',
-      name: 'Heetaku',
-      company: 'LG CNS',
-      theme: 'light',
-      title: 'Software Engineer',
-      email: 'heetaku@gmail.com',
-      message: 'mango',
-      fileName: 'heetaku',
-      fileURL: 'heetaku.png'
-    },
-    {
-      id: '3',
-      name: 'Luna',
-      company: 'LunaVillage',
-      theme: 'colorful',
-      title: 'Software Engineer',
-      email: 'luna@gmail.com',
-      message: 'mango',
-      fileName: 'luna',
-      fileURL: null
-    }
-  ])
+  const [cards, setCards] = useState({
+    1: {
+        id: '1',
+        name: 'Yooni',
+        company: 'KUUS',
+        theme: 'dark',
+        title: 'Software Engineer',
+        email: 'yooni@gmail.com',
+        message: 'mango',
+        fileName: 'yooni',
+        fileURL: null
+      },
+    2: {
+        id: '2',
+        name: 'Heetaku',
+        company: 'LG CNS',
+        theme: 'light',
+        title: 'Software Engineer',
+        email: 'heetaku@gmail.com',
+        message: 'mango',
+        fileName: 'heetaku',
+        fileURL: 'heetaku.png'
+      },
+    3: {
+        id: '3',
+        name: 'Luna',
+        company: 'LunaVillage',
+        theme: 'colorful',
+        title: 'Software Engineer',
+        email: 'luna@gmail.com',
+        message: 'mango',
+        fileName: 'luna',
+        fileURL: null
+      }
+  })
 
   const navigate = useNavigate();
 
@@ -60,16 +60,27 @@ const Maker = ({authService}) => {
     })
   });
 
-  const addCard = (card) => {
-    const updated = [...cards, card];
-    setCards(updated);
+  const createOrUpdateCard = (card) => {
+    setCards((cards) => {
+      const updated={...cards};
+      updated[card.id] = card;
+      return updated;
+    });
+  }
+
+  const deleteCard = (card) => {
+    setCards((cards) => {
+      const updated={...cards};
+      delete updated[card.id];
+      return updated;
+    });
   }
 
   return (
     <section className={styles.maker}>
       <Header onLogout={onLogout}/>
       <div className={styles.container}>
-        <Editor cards={cards} addCard={addCard} />
+        <Editor cards={cards} addCard={createOrUpdateCard} updateCard={createOrUpdateCard} deleteCard={deleteCard} />
         <Preview cards={cards} />
       </div>
       <Footer />
